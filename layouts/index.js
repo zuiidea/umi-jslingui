04d3a72ecc5e4f5b06265db09b1d60c0bdc47c2a
zuiidea <zuiiidea@gmail.com>
2018-09-05 09:57:05 +0800

    Initialize commit

---
+++ layouts/index.js
@@ -0,0 +1,15 @@
+import withRouter from 'umi/withRouter'
+import { I18nProvider } from '@lingui/react'
+import { catalogs, langFromPath } from '../utils/i18n-config'
+
+export default withRouter(props => {
+  const lang = langFromPath(props.location.pathname)
+
+  console.log(lang)
+
+  return (
+    <I18nProvider language={lang} catalogs={catalogs}>
+      {props.children}
+    </I18nProvider>
+  )
+})
---
+++ utils/i18n-config.js
@@ -0,0 +1,18 @@
+const languages = [ 'en', 'zh' ]
+const catalogs = {
+  en: { messages: require('../locales/en/messages.json') },
+  zh: { messages: require('../locales/zh/messages.json') },
+}
+
+const defaultLanguage = 'en'
+
+const prefix = (lang) => (lang === defaultLanguage ? '/' : '/' + lang)
+const deprefix = (pathname) => (pathname.startsWith('/en/') ? pathname.substr(4) : pathname)
+const langFromPath = (pathname) => (pathname.startsWith('/zh/') ? 'zh' : 'en')
+
+exports.defaultLanguage = defaultLanguage
+exports.languages = languages
+exports.catalogs = catalogs
+exports.prefix = prefix
+exports.deprefix = deprefix
+exports.langFromPath = langFromPath
