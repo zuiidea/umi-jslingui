86728e3cb765758e9cf43aac8948c83ac0a15179
zuiidea <zuiiidea@gmail.com>
2018-09-06 00:02:59 +0800

    add travis

--- src/utils/i18n-config.js
+++ src/utils/i18n-config.js
@@ -1,17 +1,15 @@
-const languages = [ 'en', 'zh' ]
+const languages = ['en', 'zh']
 const catalogs = {
   en: { messages: require('../locales/en/messages.json') },
   zh: { messages: require('../locales/zh/messages.json') },
 }
 
 const defaultLanguage = 'en'
-const deprefix = (pathname) => `/${pathname.split('/').slice(2).join('/')}`
-const langFromPath = (pathname) => (pathname.startsWith('/zh/') ? 'zh' : 'en')
+const deprefix = pathname =>
+  `/${pathname
+    .split('/')
+    .slice(2)
+    .join('/')}`
+const langFromPath = pathname => (pathname.startsWith('/zh/') ? 'zh' : 'en')
 
-export {
-  languages,
-  catalogs,
-  langFromPath,
-  deprefix,
-  defaultLanguage,
-}
+export { languages, catalogs, langFromPath, deprefix, defaultLanguage }
